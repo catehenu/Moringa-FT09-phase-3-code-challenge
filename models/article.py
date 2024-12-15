7e5df3209d7fab8e1d8228c42b9b7c567e957781
cate henu <catherine.henu@student.moringaschool.com>
2024-12-15 18:47:13 +0300

    updated files

--- models/article.py
+++ models/article.py
@@ -1,10 +1,81 @@
+from database.connection import get_db_connection
+
 class Article:
-    def __init__(self, id, title, content, author_id, magazine_id):
-        self.id = id
+    def __init__(self, article_id=None, title=None, content=None, author=None, magazine=None):
+        self.article_id = article_id
         self.title = title
         self.content = content
-        self.author_id = author_id
-        self.magazine_id = magazine_id
+        self.author = author
+        self.magazine = magazine
+
+        if self.article_id is None and self.title is not None:
+            self.create_article()
+        elif self.article_id is not None:
+            self.load_article_data()
+
+    def create_article(self):
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        cursor.execute(
+            'INSERT INTO articles (title, content, author_id, magazine_id) VALUES (?, ?, ?, ?)',
+            (self.title, self.content, self.author, self.magazine)
+        )
+
+        self.article_id = cursor.lastrowid
+
+        connection.commit()
+        connection.close()
+
+    @property
+    def article_title(self):
+        if not hasattr(self, '_title') and self.article_id is not None:
+            self.load_article_data()
+        return self.title
+
+    @article_title.setter
+    def article_title(self, new_title):
+        if self.article_id is not None:
+            raise ValueError("Cannot modify title of an existing article.")
+        if isinstance(new_title, str) and 5 <= len(new_title) <= 50:
+            self.title = new_title
+        else:
+            raise ValueError("Title must be a string with 5-50 characters.")
+
+    @property
+    def article_content(self):
+        if not hasattr(self, '_content') and self.article_id is not None:
+            self.load_article_data()
+        return self.content
+
+    @article_content.setter
+    def article_content(self, new_content):
+        if self.article_id is not None:
+            raise ValueError("Cannot modify content of an existing article.")
+        if isinstance(new_content, str):
+            self.content = new_content
+        else:
+            raise ValueError("Content must be a string.")
+
+    def load_article_data(self):
+        if self.article_id is None:
+            raise ValueError("Cannot load data without an article ID.")
+
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        cursor.execute(
+            'SELECT title, content, author_id, magazine_id FROM articles WHERE id = ?',
+            (self.article_id,)
+        )
+        row = cursor.fetchone()
+
+        if row:
+            self.title, self.content, self.author, self.magazine = row
+        else:
+            raise ValueError("No article found with the given ID.")
+
+        connection.close()
 
     def __repr__(self):
-        return f'<Article {self.title}>'
+        return f"<Article: {self.title}>"
--- models/author.py
+++ models/author.py
@@ -1,7 +1,68 @@
+from database.connection import get_db_connection
+
 class Author:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
+    def __init__(self, author_id, name):
+        self.author_id = author_id
+        self.name = name  # This will use the setter method
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string.")
+        if not (2 <= len(value) <= 50):  # Assuming a length validation
+            raise ValueError("Name must be between 2 and 50 characters.")
+        self._name = value
+
+
+
+    def _retrieve_name_from_db(self):
+        if self.author_id is None:
+            raise ValueError("Author ID is required to retrieve the name from the database.")
+
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute('SELECT name FROM authors WHERE id = ?', (self.author_id,))
+
+        result = cursor.fetchone()
+        if result:
+            self.full_name = result[0]
+        else:
+            raise ValueError(f"No author found with ID {self.author_id}.")
+
+        connection.close()
+
+    def fetch_articles(self):
+        from models.article import Article
+
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        cursor.execute('SELECT * FROM articles WHERE author_id = ?', (self.author_id,))
+        articles_data = cursor.fetchall()
+        connection.close()
+
+        return [Article(article['id'], article['title'], article['content'], article['author_id'], article['magazine_id']) for article in articles_data]
+
+    def fetch_magazines(self):
+        from models.magazine import Magazine
+
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        cursor.execute('''
+            SELECT DISTINCT magazines.* FROM magazines
+            JOIN articles ON magazines.id = articles.magazine_id
+            WHERE articles.author_id = ?
+        ''', (self.author_id,))
+
+        magazines_data = cursor.fetchall()
+        connection.close()
+
+        return [Magazine(magazine['id'], magazine['name'], magazine['category']) for magazine in magazines_data]
 
     def __repr__(self):
-        return f'<Author {self.name}>'
+        return f"<Author: {self.name}>"
